3bd08b2b1e6beb80ffe229d4bbb13daee0451044
Aine Kearns <aine.kearns@hotmail.ca>
2020-10-07 10:28:45 +0200

    spring initialized project

---
+++ src/main/java/com/challenge/cubistic/CubisticApplication.java
@@ -0,0 +1,13 @@
+package com.challenge.cubistic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CubisticApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CubisticApplication.class, args);
+	}
+
+}
